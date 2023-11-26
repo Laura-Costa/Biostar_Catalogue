405f2edf4f67c8f19316b594cf2d124f061d0b56
Laura-Costa <l.serranocosta@gmail.com>
2023-11-26 15:09:54 -0300

    .

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot
 #import numpy as np
 import csv
 
-connection = mysql.connector.connect(host='localhost', port='3306', database='catalogo_gaia', user='helena', password='ic2023')
+connection = mysql.connector.connect(host='localhost', port='3306', database='catalogo_gaia', user='helena', password='IC2023ic*')
 
 cursor = connection.cursor()
 
@@ -17,125 +17,16 @@ def index():
 
 @app.route("/tabela_de_dados/")
 def tabela_de_dados():
-    cursor.execute("select numero_ordinal_do_registro, designation, ruwe, parallax, parallax_error, phot_g_mean_mag, phot_bp_mean_mag, phot_rp_mean_mag, teff_gspphot, teff_gspphot_lower, teff_gspphot_upper, logg_gspphot, logg_gspphot_lower, logg_gspphot_upper, mh_gspphot, mh_gspphot_lower, mh_gspphot_upper, distance_gspphot, distance_gspphot_lower, distance_gspphot_upper from Source order by numero_ordinal_do_registro")
+    cursor.execute("select * from Source_Gaia order by numero_ordinal_do_registro")
     value = cursor.fetchall()
 
-    # não são produtos
-    numero_ordinal_do_registro = []
-    designation = []
-    ruwe = []
-    parallax = []
-    parallax_error = []
-    phot_g_mean_mag = []
-    phot_bp_mean_mag = []
-    phot_rp_mean_mag = []
-    teff_gspphot = []
-    teff_gspphot_lower = []
-    teff_gspphot_upper = []
-    logg_gspphot = []
-    logg_gspphot_lower = []
-    logg_gspphot_upper = []
-    mh_gspphot = []
-    mh_gspphot_lower = []
-    mh_gspphot_upper = []
-    distance_gspphot = []
-    distance_gspphot_lower = []
-    distance_gspphot_upper = []
-
-    for star in value:
-        numero_ordinal_do_registro.append(star[0])
-        designation.append(star[1])
-        ruwe.append(star[2])
-        parallax.append(star[3])
-        parallax_error.append(star[4])
-        phot_g_mean_mag.append(star[5])
-        phot_bp_mean_mag.append(star[6])
-        phot_rp_mean_mag.append(star[7])
-        teff_gspphot.append(star[8])
-        teff_gspphot_lower.append(star[9])
-        teff_gspphot_upper.append(star[10])
-        logg_gspphot.append(star[11])
-        logg_gspphot_lower.append(star[12])
-        logg_gspphot_upper.append(star[13])
-        mh_gspphot.append(star[14])
-        mh_gspphot_lower.append(star[15])
-        mh_gspphot_upper.append(star[16])
-        distance_gspphot.append(star[17])
-        distance_gspphot_lower.append(star[18])
-        distance_gspphot_upper.append(star[19])
-
-
-    # produtos
-    indice_de_cor = []
-    mg = []
-    erro_de_mg = []
-    mg_mais = []
-    mg_menos = []
-    mrp = []
-    mrp_mais = []
-    mrp_menos = []
-    erro_de_mrp = []
-    erro_de_distancia = []
-    erro_de_teff = []
-    erro_de_logg = []
-    erro_de_mh = []
-
-    cont = 0
-    for star in value:
-        indice_de_cor.append(star[6] - star[7])
-        mg.append(star[5] + 5 + 5*math.log((star[3])/1000, 10))
-        mg_mais.append(star[5] + 5 + 5*math.log((star[3]+star[4])/1000, 10))
-        mg_menos.append(star[5] + 5 + 5 * math.log((star[3] - star[4])/1000, 10))
-        erro_de_mg.append((abs(mg[cont] - mg_mais[cont]) + abs(mg[cont] - mg_menos[cont]))/2)
-        mrp.append(star[7] + 5 + 5*math.log((star[3])/1000, 10))
-        mrp_mais.append(star[7] + 5 + 5*math.log((star[3] + star[4])/1000, 10))
-        mrp_menos.append(star[7] + 5 + 5 * math.log((star[3] - star[4])/1000, 10))
-        erro_de_mrp.append((abs(mrp[cont] - mrp_mais[cont]) + abs(mrp[cont] - mrp_menos[cont]))/2)
-        erro_de_distancia.append((abs(star[17] - star[18]) + abs(star[17] - star[19]))/2)
-        erro_de_teff.append((abs(star[8] - star[9]) + abs(star[8] - star[10]))/2)
-        erro_de_logg.append((abs(star[11] - star[12]) + abs(star[11] - star[13]))/2)
-        erro_de_mh.append((abs(star[14] - star[15]) + abs(star[14] - star[16]))/2)
-
-        cont += 1
-
-    # exportar listas para csv
-
-    #rows = zip(numero_ordinal_do_registro, phot_g_mean_mag, parallax, phot_bp_mean_mag, phot_rp_mean_mag, mg, indice_de_cor)
-    #with open("/home/h/Área de trabalho/Catalogo_GAIA/dados_gaia.csv", "w") as f:
-    #    writer = csv.writer(f)
-    #    for row in rows:
-    #        writer.writerow(row)
-
-    return render_template("tabela_de_dados.html", data=(numero_ordinal_do_registro, designation, ruwe, phot_g_mean_mag, phot_rp_mean_mag, indice_de_cor, mg, erro_de_mg, mrp, erro_de_mrp, distance_gspphot, erro_de_distancia, teff_gspphot, erro_de_teff, logg_gspphot, erro_de_logg, mh_gspphot, erro_de_mh), name='Tabela de Dados',zip=zip)
+    return render_template("tabela_de_dados.html", data=value, name='Tabela de Dados')
 
 @app.route("/diagramas_gaia/")
 def diagramas_gaia():
-    cursor.execute("select parallax, phot_g_mean_mag, phot_bp_mean_mag, phot_rp_mean_mag from Source order by numero_ordinal_do_registro")
+    cursor.execute("select parallax, phot_g_mean_mag, phot_bp_mean_mag, phot_rp_mean_mag from Source_Gaia order by numero_ordinal_do_registro")
     value = cursor.fetchall()
-
-    # não são produtos
-    parallax = []
-    phot_g_mean_mag = []
-    phot_bp_mean_mag = []
-    phot_rp_mean_mag = []
-
-    # são produtos
-    mg = []
-    mrp = []
-    indice_de_cor = []
-
-    cont = 0
-    for star in value:
-        parallax.append(star[0])
-        phot_g_mean_mag.append(star[1])
-        phot_bp_mean_mag.append(star[2])
-        phot_rp_mean_mag.append(star[3])
-
-        mg.append(phot_g_mean_mag[cont] + 5 + 5*math.log((parallax[cont])/1000, 10))
-        mrp.append(phot_rp_mean_mag[cont] + 5 + 5 * math.log((parallax[cont])/1000, 10))
-        indice_de_cor.append(phot_bp_mean_mag[cont] - phot_rp_mean_mag[cont])
-        cont += 1
-
+    '''
     transparency = 1
     size = 1.5
 
@@ -156,60 +47,15 @@ def diagramas_gaia():
     matplotlib.pyplot.xlabel("BP-RP")
     matplotlib.pyplot.savefig('static/images/mrp_indice_de_cor.png')
     matplotlib.pyplot.clf()
+    '''
+    return render_template("diagramas_gaia.html", data=([3], [3], [3]), name='Diagramas GAIA', zip=zip)
 
-    return render_template("diagramas_gaia.html", data=(mg, mrp, indice_de_cor), name='Diagramas GAIA', zip=zip)
 
 @app.route("/diagramas_hipparcos/")
 def diagramas_hipparcos():
     cursor.execute("select Vmag, Plx, BTmag, VTmag, B_V, numero_ordinal_do_registro from Source_Hipparcos order by numero_ordinal_do_registro")
     value = cursor.fetchall()
-
-    # não são produtos
-    Vmag = []
-    Plx = []
-    BTmag = []
-    VTmag = []
-    B_V = []
-    numero_ordinal_do_registro = []
-
-    # são produtos
-    MVt = []
-    MV = []
-    BT_VT = []
-    B_V_diagrama = []
-    numero_ordinal_do_registro_diagrama = []
-
-    cont = 0
-    qtde_de_registros_usados = 0
-    for star in value:
-        Vmag.append(star[0])
-        Plx.append(star[1])
-        BTmag.append(star[2])
-        VTmag.append(star[3])
-        B_V.append(star[4])
-        numero_ordinal_do_registro.append(star[5])
-
-        paralaxe_minima = 0.0500 #dá a distância máxima
-        if((Plx[cont]/1000 > paralaxe_minima or Plx[cont]/1000 == paralaxe_minima) and (BTmag[cont] - VTmag[cont]) != 0.000 and B_V[cont] != 0.000):
-            qtde_de_registros_usados += 1
-            MVt.append(VTmag[cont] + 5 + 5*math.log(Plx[cont]/1000, 10))
-            MV.append(Vmag[cont] + 5 + 5 * math.log(Plx[cont]/1000, 10))
-            BT_VT.append(BTmag[cont] - VTmag[cont])
-            B_V_diagrama.append(B_V[cont])
-            numero_ordinal_do_registro_diagrama.append(numero_ordinal_do_registro[cont])
-
-        cont += 1
-
-
-    # exportar listas para csv
-
-    #rows = zip(numero_ordinal_do_registro_diagrama, MV, B_V_diagrama, MVt, BT_VT)
-    #with open("/home/h/Área de trabalho/Catalogo_GAIA/dados_hipparcos.csv", "w") as f:
-    #    writer = csv.writer(f)
-    #    for row in rows:
-    #        writer.writerow(row)
-
-
+    '''
     transparency = 1
     size = 1.5
 
@@ -230,8 +76,8 @@ def diagramas_hipparcos():
     matplotlib.pyplot.xlabel("BT-VT")
     matplotlib.pyplot.savefig('static/images/MVt_BT_VT.png')
     matplotlib.pyplot.clf()
-
-    return render_template("diagramas_hipparcos.html", data=(MV, B_V_diagrama, MVt, BT_VT, numero_ordinal_do_registro_diagrama), name='Diagramas Hipparcos', zip=zip)
+    '''
+    return render_template("diagramas_hipparcos.html", data=([2], [2], [2], [2], [2]), name='Diagramas Hipparcos', zip=zip)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- modelo_fisico.sql
+++ modelo_fisico.sql
@@ -1,33 +1,164 @@
-
-Aqui os tipos foram colocados tudo double:
-
-CREATE TABLE Source (
+create table Source_Gaia (
 	numero_ordinal_do_registro int not null auto_increment unique,
-	designation char(100) PRIMARY KEY,
-	ruwe double not null,
+	solution_id long not null,
+	designation char(100),
+	source_id long not null,
+	random_index long not null,
+	ref_epoch double not null,
+	ra double not null,
+	ra_error float not null,
+	declination double not null,
+	dec_error float not null,
 	parallax double not null,
-	parallax_error double not null,
-	phot_g_mean_mag double not null,
-	phot_bp_mean_mag double not null,
-	phot_rp_mean_mag double not null,
-	teff_gspphot double not null,
-	teff_gspphot_lower double not null,
-	teff_gspphot_upper double not null,
-	logg_gspphot double not null,
-	logg_gspphot_lower double not null,
-	logg_gspphot_upper double not null,
-	mh_gspphot double not null,
-	mh_gspphot_lower double not null,
-	mh_gspphot_upper double not null,
-	distance_gspphot double not null,
-	distance_gspphot_lower double not null,
-	distance_gspphot_upper double not null
+	parallax_error float not null,
+	parallax_over_error float not null,
+	pm float not null,
+	pmra double not null,
+	pmra_error float not null,
+	pmdec double not null,
+	pmdec_error float not null,
+	ra_dec_corr float not null,
+	ra_parallax_corr float not null,
+	ra_pmra_corr float not null,
+	ra_pmdec_corr float not null,
+	dec_parallax_corr float not null,
+	dec_pmra_corr float not null,
+	dec_pmdec_corr float not null,
+	parallax_pmra_corr float not null,
+	parallax_pmdec_corr float not null,
+	pmra_pmdec_corr float not null,
+	astrometric_n_obs_al int not null,
+	astrometric_n_obs_ac int not null,
+	astrometric_n_good_obs_al int not null,
+	astrometric_n_bad_obs_al int not null,
+	astrometric_gof_al float not null,
+	astrometric_chi2_al float not null,
+	astrometric_excess_noise float not null,
+	astrometric_excess_noise_sig float not null,
+	astrometric_params_solved int not null,
+	astrometric_primary_flag boolean not null,
+	nu_eff_used_in_astrometry float not null,
+	pseudocolour float not null,
+	pseudocolour_error float not null,
+	ra_pseudocolour_corr float not null,
+	dec_pseudocolour_corr float not null,
+	parallax_pseudocolour_corr float not null,
+	pmra_pseudocolour_corr float not null,
+	pmdec_pseudocolour_corr float not null,
+	astrometric_matched_transits int not null,
+	visibility_periods_used int not null,
+	astrometric_sigma5d_max float not null,
+	matched_transits int not null,
+	new_matched_transits int not null,
+	matched_transits_removed int not null,
+	ipd_gof_harmonic_amplitude float not null,
+	ipd_gof_harmonic_phase float not null,
+	ipd_frac_multi_peak int not null,
+	ipd_frac_odd_win int not null,
+	ruwe float not null,
+	scan_direction_strength_k1 float not null,
+	scan_direction_strength_k2 float not null,
+	scan_direction_strength_k3 float not null,
+	scan_direction_strength_k4 float not null,
+	scan_direction_mean_k1 float not null,
+	scan_direction_mean_k2 float not null,
+	scan_direction_mean_k3 float not null,
+	scan_direction_mean_k4 float not null,
+	duplicated_source boolean not null,
+	phot_g_n_obs int not null,
+	phot_g_mean_flux double not null,
+	phot_g_mean_flux_error float not null,
+	phot_g_mean_flux_over_error float not null,
+	phot_g_mean_mag float not null,
+	phot_bp_n_obs int not null,
+	phot_bp_mean_flux double not null,
+	phot_bp_mean_flux_error float not null,
+	phot_bp_mean_flux_over_error float not null,
+	phot_bp_mean_mag float not null,
+	phot_rp_n_obs int not null,
+	phot_rp_mean_flux double not null,
+	phot_rp_mean_flux_error float not null,
+	phot_rp_mean_flux_over_error float not null,
+	phot_rp_mean_mag float not null,
+	phot_bp_rp_excess_factor float not null,
+	phot_bp_n_contaminated_transits int not null,
+	phot_bp_n_blended_transits int not null,
+	phot_rp_n_contaminated_transits int not null,
+	phot_rp_n_blended_transits int not null,
+	phot_proc_mode int not null,
+	bp_rp float not null,
+	bp_g float not null,
+	g_rp float not null,
+	radial_velocity float not null,
+	radial_velocity_error float not null, 
+	rv_method_used int not null, 	
+	rv_nb_transits int not null, 
+	rv_nb_deblended_transits int not null,
+	rv_visibility_periods_used int not null, 
+	rv_expected_sig_to_noise float not null, 
+	rv_renormalised_gof float not null, 
+	rv_chisq_pvalue float not null, 
+	rv_time_duration float not null,
+	rv_amplitude_robust float not null,
+	rv_template_teff float not null,
+	rv_template_logg float not null,
+	rv_template_fe_h float not null,
+	rv_atm_param_origin int not null,
+	vbroad float not null,
+	vbroad_error float not null,
+	vbroad_nb_transits int not null,
+	grvs_mag float not null,
+	grvs_mag_error float not null,
+	grvs_mag_nb_transits int not null,
+	rvs_spec_sig_to_noise float not null,
+	phot_variable_flag char(100) not null,
+	galactic_longitude double not null,
+	galactic_latitude double not null,
+	ecl_lon double not null,
+	ecl_lat double not null,
+	in_qso_candidates boolean not null,
+	in_galaxy_candidates boolean not null,
+	non_single_star int not null,
+	has_xp_continuous boolean not null,
+	has_xp_sampled boolean not null,
+	has_rvs boolean not null,
+	has_epoch_photometry boolean not null,
+	has_epoch_rv boolean not null,
+	has_mcmc_gspphot boolean not null,
+	has_mcmc_msc boolean not null,
+	in_andromeda_survey boolean not null,
+	classprob_dsc_combmod_quasar float not null,
+	classprob_dsc_combmod_galaxy float not null,
+	classprob_dsc_combmod_star float not null,
+	teff_gspphot float not null,
+	teff_gspphot_lower float not null,
+	teff_gspphot_upper float not null,
+	logg_gspphot float not null,
+	logg_gspphot_lower float not null, 
+	logg_gspphot_upper float not null,
+	mh_gspphot float not null,
+	mh_gspphot_lower float not null,
+	mh_gspphot_upper float not null,
+	distance_gspphot float not null,
+	distance_gspphot_lower float not null,
+	distance_gspphot_upper float not null,
+	azero_gspphot float not null,
+	azero_gspphot_lower float not null,
+	azero_gspphot_upper float not null,
+	ag_gspphot float not null,
+	ag_gspphot_lower float not null,
+	ag_gspphot_upper float not null,
+	ebpminrp_gspphot float not null,
+	ebpminrp_gspphot_lower float not null,
+	ebpminrp_gspphot_upper float not null,
+	libname_gspphot char(100) not null,
+	primary key(ra, declination)
 );
 
 
 
-CREATE TABLE Source_Hipparcos (
-	numero_ordinal_do_registro int auto_increment PRIMARY KEY,
+create table Source_Hipparcos (
+	numero_ordinal_do_registro int not null auto_increment unique,
 	Catalog double not null, 
 	HIP double not null, 	
 	Proxy double not null, 
@@ -105,8 +236,71 @@ CREATE TABLE Source_Hipparcos (
 	CPD double not null, 
 	V_Ired double not null, 
 	SpType double not null, 
-	r_SpType double not null
+	r_SpType double not null,
+	primary key(RAdeg, DEdeg)	
 );	
+
+
+create table Produto_Gaia (
+     ra double not null,
+     declination double not null,
+     Mg double not null,
+     MRp double not null,
+     Bp_menos_Rp double not null,
+     primary key (ra, declination),
+     foreign key (ra, declination) references Source_Gaia(ra, declination)
+);
+
+create table Produto_Hipparcos(
+      RAdeg double not null,
+      DEdeg double not null,
+      MV double not null,
+      MVt double not null,
+      B_menos_V double not null,
+      BT_menos_VT double not null,
+      primary key (RAdeg, DEdeg),
+      foreign key (RAdeg, DEdeg) references Source_Hipparcos(RAdeg, DEdeg)
+);
+
+
+create table Diagrama_Gaia(
+      codigo char(10) not null,
+      diagrama blob not null,
+      descricao char(100),
+      primary key (codigo)
+);
+
+
+
+create table Diagrama_Hipparcos(
+       codigo char(10) not null,
+       diagrama blob not null,
+       descricao char(100),
+       primary key (codigo)
+);
+
+
+create table Produto_Gaia_é_plotado_em(
+      parallax double not null,
+      ra double not null,
+      declination double not null,
+      codigo char(10) not null,
+      primary key (ra, declination, parallax, codigo),
+      foreign key (ra, declination) references Produto_Gaia(ra, declination),
+      foreign key (codigo) references Diagrama_Gaia(codigo)
+);
+
+
+create table Produto_Hipparcos_é_plotado_em(
+      parallax double not null,
+      RAdeg double not null,
+      DEdeg double not null,
+      codigo char(10) not null,
+      primary key (RAdeg, DEdeg, parallax, codigo),
+      foreign key (RAdeg, DEdeg) references Produto_Hipparcos(RAdeg, DEdeg),
+      foreign key (codigo) references Diagrama_Hipparcos(codigo)
+);
+
 
 
 
