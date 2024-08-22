ac00dd24560ecadf6976b841ab3debdcdd1f3d5c
Laura-Costa <l.serranocosta@gmail.com>
2024-08-22 08:27:17 -0300

    depois de colocar a grid mais detalhada

--- Biostar_Catalogue/edited_diagram.py
+++ Biostar_Catalogue/edited_diagram.py
@@ -219,37 +219,50 @@ for (HIP_value, Plx_value, B_minus_V_value, MV_value) in value:
     Plx_list.append(Plx_value)
     x_axis.append(B_minus_V_value)
     y_axis.append(MV_value)
-
 min_Plx = min(Plx_list)
 
 fig, ax = plt.subplots()
-
 transparency = 1
 size = 1.5
-ax.scatter(x_axis, y_axis, s=1, color='black')
-#plt.scatter(x_axis, y_axis, s = size, marker = ".", edgecolors = 'black', alpha = transparency)
+ax.scatter(x_axis, y_axis, s=0.3, color='black')
 plt.xlim(min(x_axis) - decimal.Decimal(0.2), max(x_axis) + decimal.Decimal(0.2))
 plt.ylim(max(y_axis) + decimal.Decimal(0.5), min(y_axis) - decimal.Decimal(0.5))
-#plt.title("Hipparcos - Gaia: {} estrelas em um raio de {:.4f}pc (π ≥ {:.4f}'')".format(len(value), decimal.Decimal(1.0) / (min_Plx / decimal.Decimal(1000.0)), min_Plx / decimal.Decimal(1000.0)))
-ax.set_title("Hipparcos - Gaia: {} estrelas em um raio de {:.4f}pc (π ≥ {:.4f}'')".format(len(value), decimal.Decimal(1.0) / (min_Plx / decimal.Decimal(1000.0)), min_Plx / decimal.Decimal(1000.0)))
-plt.xlabel("B - V")
-plt.ylabel("M(V)")
+ax.set_title("M(V) x B-V", fontsize=7)
+fig.suptitle("Hipparcos - Gaia: {} estrelas em um raio de {:.4f}pc (π ≥ {:.4f}'')".format(len(value), decimal.Decimal(1.0) / (min_Plx / decimal.Decimal(1000.0)), min_Plx / decimal.Decimal(1000.0)), fontsize=8)
+plt.xlabel("B - V", fontsize=7)
+plt.ylabel("M(V)", fontsize=7)
 
-ax.xaxis.set_major_locator(MultipleLocator(0.5))
-ax.xaxis.set_minor_locator(MultipleLocator(0.1))
-ax.yaxis.set_major_locator(MultipleLocator(2))
-ax.yaxis.set_minor_locator(MultipleLocator(0.5))
+# tentar colocar nos dois eixos o mesmo tamanho entre os majorticks
+print("tentar colocar nos dois eixos o mesmo tamanho entre os majorticks", (decimal.Decimal(0.5)*(abs(min(x_axis)) + abs(max(x_axis))))/(abs(min(y_axis)) + abs(max(y_axis))))
 
-ax.xaxis.set_minor_formatter(FormatStrFormatter("%.1f"))
-ax.yaxis.set_minor_formatter(FormatStrFormatter("%.1f"))
+# definir os intervalos dos minor e major ticks, dos eixos x e eixos y
+ax.xaxis.set_major_locator(MultipleLocator(0.125))
+ax.xaxis.set_minor_locator(MultipleLocator(0.03125))
+ax.yaxis.set_major_locator(MultipleLocator(0.5))
+ax.yaxis.set_minor_locator(MultipleLocator(0.1))
 
-#ax.tick_params(wich='both', width=2)
-ax.tick_params(axis='both', which='major', labelsize=5)
-ax.tick_params(axis='both', which='minor', labelsize=2.5, labelcolor='dimgray')
+# para colocar label no minor axis também
+# ax.xaxis.set_minor_formatter(FormatStrFormatter("%.1f"))
+# ax.yaxis.set_minor_formatter(FormatStrFormatter("%.1f"))
 
+# configurar labels dos major e minor ticks de ambos os eixos
+ax.tick_params(axis='both', which='major', labelsize=3)
+# ax.tick_params(axis='both', which='minor', labelsize=2.5, labelcolor='dimgray')
+
+# rotacionar label do eixo x
+plt.xticks(rotation=45)
+
+# colocar a grid atras do plot
 ax.set_axisbelow(True)
-plt.grid(color='darkgrey', linestyle='dashed', which='major', linewidth=0.5)
-plt.grid(color='lightgray', linestyle='dashed', which='minor', linewidth=0.5)
+
+# deixar o axes com aspecto quadrado
+ax.set_box_aspect(1)
+
+# configurar as caracteristicas da grid
+plt.grid(color='darkgrey', linestyle='dashed', dashes=(5,5), which='major', linewidth=0.2)
+# plt.grid(color='lightgray', linestyle='dashed', dashes=(5,5), which='minor', linewidth=0.2)
+
+# salvar diagrama
 plt.savefig('/home/lh/Desktop/Catalogo_GAIA/Biostar_Catalogue/static/img/Hipparcos_minus_Gaia_MV_versus_B_minus_V_ticks.pdf')
 
 # close matplotlib.pyplot as plt object
