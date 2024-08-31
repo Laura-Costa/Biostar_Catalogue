714cae7b58b7e8db49f8aa95bd2166db1bd86678
Laura-Costa <l.serranocosta@gmail.com>
2024-08-31 18:56:21 -0300

    tentar corresponder CAT2 com Gaia todo usando seleção no Vizier dist<50pc

--- biostar_catalogue/corresponder_CAT2_gaia_todo.py
+++ biostar_catalogue/corresponder_CAT2_gaia_todo.py
@@ -7,7 +7,14 @@ cursor = connection.cursor()
 
 cursor.execute("set global local_infile='ON'")
 
-cursor.execute("drop table if exists Gaia_DR3")
+
+
+
+
+
+
+#######################################################################################
+# consulta para saber quantos HIP têm Gaia DR3
 
 cursor.execute("select Hipparcos.HIP "
                "from Hipparcos ")
@@ -29,6 +36,11 @@ for i in range(len(HIP_list)):
         hips_com_designacao_DR3 += (str(HIP_list[i]),)
 
 print(len(hips_com_designacao_DR3))
+#######################################################################################
+
+
+
+cursor.execute("drop table if exists Gaia_DR3")
 
 # Criar a tabela Gaia_DR3 no BD:
 
@@ -78,26 +90,184 @@ with open("asu.tsv") as tsv:
                 cursor.execute("update Gaia_DR3 set HIP = {} where designation = '{}'".format(int(HIP_value), designation_value))
 tsv.close()
 
-# cursor.execute("drop table Gaia_DR3")
+#cursor.execute("drop table Gaia_DR3")
+
+cursor.execute("drop table if exists Hipparcos_DR3")
+
+# Criar a tabela Hipparcos_DR3 no BD:
+
+cursor.execute("create table Hipparcos_DR3("
+               "HIP INT primary key,"
+               "HD CHAR(100) null,"
+               "BD CHAR(100) null,"
+               "CoD CHAR(100) null,"
+               "CPD CHAR(100) null,"
+               "Vmag NUMERIC(65,30) null,"
+               "RAdeg NUMERIC(65,30) null,"
+               "DEdeg NUMERIC(65,30) null,"
+               "RAhms CHAR(100) null,"
+               "DEdms CHAR(100) null,"
+               "Plx NUMERIC(65,30) null,"
+               "e_Plx NUMERIC(65,30) null,"
+               "pmRA NUMERIC(65,30) null,"
+               "pmDE NUMERIC(65,30) null,"
+               "BTmag NUMERIC(65,30) null,"
+               "VTmag NUMERIC(65,30) null,"
+               "B_V NUMERIC(65,30) null,"
+               "SpType CHAR(100) null,"
+               "designation_DR3 CHAR(100) null)")
+
+# Carregar os dados da tabela Hipparcos:
+
+with open("HIP_MAIN.DAT") as tsv:
+    for line in csv.reader(tsv, dialect="excel-tab"):
+        if len(line) != 0:
+            line = line[0].split("|")
+            if len(line[11].strip()) != 0 and float(line[11].strip()) >= 0.039 * 1000.0:
+
+                HIP_value = line[1].strip()
+                if len(HIP_value) == 0:
+                    cursor.execute("insert into Hipparcos_DR3(HIP) values(NULL)")
+                else:
+                    cursor.execute("insert into Hipparcos_DR3(HIP) values({})".format(int(HIP_value)))
+
+                HD_value = line[71].strip()
+                if len(HD_value) == 0:
+                    cursor.execute("update Hipparcos_DR3 set HD = NULL where HIP = {}".format(int(HIP_value)))
+                else:
+                    cursor.execute("update Hipparcos_DR3 set HD = {} where HIP = {}".format(HD_value, int(HIP_value)))
+
+                BD_value = line[72].strip()
+                if len(BD_value) == 0:
+                    cursor.execute("update Hipparcos_DR3 set BD = NULL where HIP = {}".format(int(HIP_value)))
+                else:
+                    cursor.execute("update Hipparcos_DR3 set BD = '{}' where HIP = {}".format(BD_value, int(HIP_value)))
+
+                CoD_value = line[73].strip()
+                if len(CoD_value) == 0:
+                    cursor.execute("update Hipparcos_DR3 set CoD = NULL where HIP = {}".format(int(HIP_value)))
+                else:
+                    cursor.execute("update Hipparcos_DR3 set CoD = '{}' where HIP = {}".format(CoD_value, int(HIP_value)))
+
+                CPD_value = line[74].strip()
+                if len(CPD_value) == 0:
+                    cursor.execute("update Hipparcos_DR3 set CPD = NULL where HIP = {}".format(int(HIP_value)))
+                else:
+                    cursor.execute("update Hipparcos_DR3 set CPD = '{}' where HIP = {}".format(CPD_value, int(HIP_value)))
+
+                Vmag_value = line[5].strip()
+                if len(Vmag_value) == 0:
+                    cursor.execute("update Hipparcos_DR3 set Vmag = NULL where HIP = {}".format(int(HIP_value)))
+                else:
+                    cursor.execute("update Hipparcos_DR3 set Vmag = {} where HIP = {}".format(float(Vmag_value), int(HIP_value)))
+
+                RAdeg_value = line[8].strip()
+                if len(RAdeg_value) == 0:
+                    cursor.execute("update Hipparcos_DR3 set RAdeg = NULL where HIP = {}".format(int(HIP_value)))
+                else:
+                    cursor.execute("update Hipparcos_DR3 set RAdeg = {} where HIP = {}".format(float(RAdeg_value), int(HIP_value)))
+
+                DEdeg_value = line[9].strip()
+                if len(DEdeg_value) == 0:
+                    cursor.execute("update Hipparcos_DR3 set DEdeg = NULL where HIP = {}".format(int(HIP_value)))
+                else:
+                    cursor.execute("update Hipparcos_DR3 set DEdeg = {} where HIP = {}".format(float(DEdeg_value), int(HIP_value)))
+
+                RAhms_value = line[3].strip()
+                if len(RAhms_value) == 0:
+                    cursor.execute("update Hipparcos_DR3 set RAhms = NULL where HIP = {}".format(int(HIP_value)))
+                else:
+                    cursor.execute("update Hipparcos_DR3 set RAhms = '{}' where HIP = {}".format(RAhms_value, int(HIP_value)))
+
+                DEdms_value = line[4].strip()
+                if len(DEdms_value) == 0:
+                    cursor.execute("update Hipparcos_DR3 set DEdms = NULL where HIP = {}".format(int(HIP_value)))
+                else:
+                    cursor.execute("update Hipparcos_DR3 set DEdms = '{}' where HIP = {}".format(DEdms_value, int(HIP_value)))
+
+                Plx_value = line[11].strip()
+                if len(Plx_value) == 0:
+                    cursor.execute("update Hipparcos_DR3 set Plx = NULL where HIP = {}".format(int(HIP_value)))
+                else:
+                    cursor.execute("update Hipparcos_DR3 set Plx = {} where HIP = {}".format(float(Plx_value), int(HIP_value)))
+
+                e_Plx_value = line[16].strip()
+                if len(e_Plx_value) == 0:
+                    cursor.execute("update Hipparcos_DR3 set e_Plx = NULL where HIP = {}".format(int(HIP_value)))
+                else:
+                    cursor.execute("update Hipparcos_DR3 set e_Plx = {} where HIP = {}".format(float(e_Plx_value), int(HIP_value)))
+
+                pmRA_value = line[12].strip()
+                if len(pmRA_value) == 0:
+                    cursor.execute("update Hipparcos_DR3 set pmRA = NULL where HIP = {}".format(int(HIP_value)))
+                else:
+                    cursor.execute("update Hipparcos_DR3 set pmRA = {} where HIP = {}".format(float(pmRA_value), int(HIP_value)))
+
+                pmDE_value = line[13].strip()
+                if len(pmDE_value) == 0:
+                    cursor.execute("update Hipparcos_DR3 set pmDE = NULL where HIP = {}".format(int(HIP_value)))
+                else:
+                    cursor.execute("update Hipparcos_DR3 set pmDE = {} where HIP = {}".format(float(pmDE_value), int(HIP_value)))
+
+                BTmag_value = line[32].strip()
+                if len(BTmag_value) == 0:
+                    cursor.execute("update Hipparcos_DR3 set BTmag = NULL where HIP = {}".format(int(HIP_value)))
+                else:
+                    cursor.execute("update Hipparcos_DR3 set BTmag = {} where HIP = {}".format(float(BTmag_value), int(HIP_value)))
+
+                VTmag_value = line[34].strip()
+                if len(VTmag_value) == 0:
+                    cursor.execute("update Hipparcos_DR3 set VTmag = NULL where HIP = {}".format(int(HIP_value)))
+                else:
+                    cursor.execute("update Hipparcos_DR3 set VTmag = {} where HIP = {}".format(float(VTmag_value), int(HIP_value)))
+
+                B_V_value = line[37].strip()
+                if len(B_V_value) == 0:
+                    cursor.execute("update Hipparcos_DR3 set B_V = NULL where HIP = {}".format(int(HIP_value)))
+                else:
+                    cursor.execute("update Hipparcos_DR3 set B_V = {} where HIP = {}".format(float(B_V_value), int(HIP_value)))
+
+                SpType_value = line[76].strip()
+                if len(SpType_value) == 0:
+                    cursor.execute("update Hipparcos_DR3 set SpType = NULL where HIP = {}".format(int(HIP_value)))
+                else:
+                    cursor.execute("update Hipparcos_DR3 set SpType = '{}' where HIP = {}".format(SpType_value, int(HIP_value)))
+
+                # Verificar se no Simbad existe um Gaia DR3 correspondente ao HIP
+                # Se houver, carregar no BD. Senao, colocar NULL.
+
+                tab = Simbad.query_objectids("HIP " + str(HIP_value))
+
+                if len([id for id in tab['ID'] if id.startswith('Gaia DR3')]) == 0:
+                    cursor.execute("update Hipparcos_DR3 set designation_DR3 = NULL where HIP = {}".format(int(HIP_value)))
+                else:
+                    designation_DR3_value = [id for id in tab['ID'] if id.startswith('Gaia DR3')][0][0:].strip()
+                    cursor.execute(
+                        "update Hipparcos_DR3 set designation_DR3 = '{}' where HIP = {}".format(designation_DR3_value, int(HIP_value)))
+tsv.close()
+
+#cursor.execute("drop table Hipparcos_DR3")
 
 # Make sure data is committed to the database
 connection.commit()
 
 # Criar o arquivo CAT2_intersecao_Gaia.csv
-hips_com_designacao_DR3 = ",".join(hips_com_designacao_DR3)
-cursor.execute('''select Hipparcos.HIP, '''
+
+'''hips_com_designacao_DR3 = ",".join(hips_com_designacao_DR3)'''
+
+cursor.execute('''select Hipparcos_DR3.HIP, '''
                '''Gaia_DR3.designation, '''
-               '''TRIM(Hipparcos.Plx)+0, '''
-               '''TRIM(Hipparcos.e_Plx)+0, '''
+               '''TRIM(Hipparcos_DR3.Plx)+0, '''
+               '''TRIM(Hipparcos_DR3.e_Plx)+0, '''
                '''TRIM(Gaia_DR3.Plx)+0 as Plx_Gaia, '''
                '''TRIM(Gaia_DR3.e_Plx)+0, '''
                '''TRIM(Gaia_DR3.distance_gspphot)+0 '''
-               '''from Hipparcos, Gaia_DR3 '''
-               '''where Hipparcos.HIP IN (%s) and ''' 
-               '''Hipparcos.HIP = Gaia_DR3.HIP '''
+               '''from Hipparcos_DR3, Gaia_DR3 '''
+               '''where Hipparcos_DR3.designation_DR3 = Gaia_DR3.designation and '''
+               '''Hipparcos_DR3.designation_DR3 is not NULL '''
                '''order by Plx_Gaia DESC '''
                '''into outfile '/var/lib/mysql-files/CAT2_intersecao_Gaia.csv' '''
-               '''fields optionally enclosed by '"' terminated by ',' LINES TERMINATED BY '\n' ''' % hips_com_designacao_DR3)
+               '''fields optionally enclosed by '"' terminated by ',' LINES TERMINATED BY '\n' ''')
 
 cursor.close()
 connection.close()
---
+++ biostar_catalogue/histograms.py
@@ -0,0 +1,117 @@
+import decimal
+import matplotlib.pyplot as plt
+import mysql.connector
+import numpy as np
+
+connection = mysql.connector.connect(host='localhost', port='3306', database='biostar_catalogue', user='lh', password='ic2023', allow_local_infile=True)
+cursor = connection.cursor()
+
+cursor.execute("set global local_infile='ON'")
+
+###############################################################################################
+cursor.execute("select Hipparcos.e_Plx, "
+               "Hipparcos.Plx "
+               "from Hipparcos ")
+value = cursor.fetchall()
+
+e_Plx_list = []
+Plx_list = []
+
+for (e_Plx_value, Plx_value) in value:
+    e_Plx_list.append(e_Plx_value)
+    Plx_list.append(Plx_value)
+
+min_Plx = min(Plx_list)
+
+# data = [erro/decimal.Decimal(1000.0) for erro in e_Plx_list]
+data = e_Plx_list
+print(data)
+print(max(data))
+print(min(data))
+binwidth = decimal.Decimal(9)
+plt.hist(data, bins=np.arange(min(data), max(data), binwidth), edgecolor="white")
+
+plt.ylabel ('quantidade de estrelas')
+plt.xlabel ('e_Plx (mas)')
+plt.title('π ≥ {:.1f} mas (CAT2, {} estrelas)'.format(min_Plx, len(data)))
+# plt.axvline(np.mean(data))
+plt.axvline(sum(data)/len(data), color='red', linestyle='dashed', linewidth=1.5, label=str('{:.5}'.format(sum(data)/len(data))))
+plt.legend(loc='upper right')
+plt.savefig('/home/lh/Desktop/Catalogo_GAIA/biostar_catalogue/static/img/histograma_e_Plx_CAT2.pdf')
+plt.close()
+###############################################################################################
+
+# cria os subplots
+fig, axes = plt.subplots(figsize=(50, 25))
+
+# plot
+axes.hist(data, edgecolor="white", color="blue")
+axes.set_xlim((min(data)-decimal.Decimal(0.001), max(data)+decimal.Decimal(0.001)))
+
+plt.savefig('/home/lh/Desktop/Catalogo_GAIA/biostar_catalogue/static/img/histograma2_e_Plx_CAT2.pdf')
+plt.close()
+###############################################################################################
+
+###############################################################################################
+cursor.execute("select Gaia.parallax_error, "
+               "Gaia.parallax "
+               "from Gaia "
+               "where Gaia.parallax < 50.000 and "
+               "Gaia.parallax + 5*Gaia.parallax_error >= 50.000")
+value = cursor.fetchall()
+print('value: ', value)
+parallax_error_list = []
+
+for (parallax_error_value, parallax_value) in value:
+    parallax_error_list.append(parallax_error_value)
+
+data = parallax_error_list
+print('data: ', data)
+print(max(data))
+print(min(data))
+binwidth = decimal.Decimal(0.005)
+plt.hist(data, bins=len(data), edgecolor="white")
+
+plt.ylabel ('quantidade de estrelas')
+plt.xlabel ('parallax_error (mas)')
+plt.title('parallax_error (CAT1, {} estrelas)'.format(len(data)))
+# plt.axvline(np.mean(data))
+plt.axvline(sum(data)/len(data), color='red', linestyle='dashed', linewidth=1.5, label=str('{:.5}'.format(np.mean(data))))
+plt.legend(loc='upper right')
+plt.savefig('/home/lh/Desktop/Catalogo_GAIA/biostar_catalogue/static/img/histograma_parallax_error_CAT1_5_sigma.pdf')
+plt.close()
+###############################################################################################
+
+###############################################################################################
+cursor.execute("select Gaia.parallax_error, "
+               "Gaia.parallax "
+               "from Gaia "
+               "where Gaia.parallax < 50.000 and "
+               "Gaia.parallax + 10*Gaia.parallax_error >= 50.000")
+value = cursor.fetchall()
+print('value: ', value)
+parallax_error_list = []
+
+for (parallax_error_value, parallax_value) in value:
+    parallax_error_list.append(parallax_error_value)
+
+data = parallax_error_list
+print('data: ', data)
+print(max(data))
+print(min(data))
+binwidth = decimal.Decimal(0.048)
+plt.hist(data, bins=np.arange(min(data), max(data), binwidth), edgecolor="white")
+
+plt.ylabel ('quantidade de estrelas')
+plt.xlabel ('parallax_error (mas)')
+plt.title('π < 50.000 mas & π ≥ 50.000 + 10σ mas (CAT1, {} estrelas)'.format(len(data)))
+#plt.title('parallax_error (CAT1)')
+# plt.axvline(np.mean(data))
+plt.axvline(sum(data)/len(data), color='red', linestyle='dashed', linewidth=1.5, label=str('{:.5}'.format(np.mean(data))))
+plt.legend(loc='upper right')
+plt.savefig('/home/lh/Desktop/Catalogo_GAIA/biostar_catalogue/static/img/histograma_parallax_error_CAT1_10_sigma.pdf')
+plt.close()
+###############################################################################################
+
+cursor.close()
+connection.close()
